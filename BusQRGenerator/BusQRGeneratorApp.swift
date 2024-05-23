b45729dd0e6a16931a7b3a1079e6c04081306c60
Omar Vidaña Rodríguez <omviro@gmail.com>
2024-05-23 14:36:38 -0600

    Initial Commit

---
+++ BusQRGenerator/BusQRGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusQRGeneratorApp.swift
+//  BusQRGenerator
+//
+//  Created by Alumno-008 on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BusQRGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
